ad0cc7dafaa939393bd056df5e307558c4750224
Ayy3 <mauricetteaustin@gmail.com>
2018-11-08 20:26:05 -0500

    Added password requirements on backend

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -18,14 +18,47 @@ router.get('/', (req, res) => {
 // @access public for now, need to add authentication, password requirements ***
 // @will setup to encrypt passwords
 router.post('/', (req, res) => {
+	var lowerCase = /[a-z]/g;
+	var upperCase = /[A-Z]/g;
+	var numbers = /[0-9]/g;
+	var pass = req.body.password;
+	var valid = true;
+	var badPass = "Cannot create account: Password needs";
+	
+	if(lowerCase.test(pass) === false){
+		console.log(1);
+		valid = false;
+		badPass = badPass + " lowercase characters,";
+	}
+	if(upperCase.test(pass) === false){
+		console.log(2);
+		valid = false;
+		badPass = badPass + " uppercase characters,";
+	}
+	if(numbers.test(pass) === false){
+		console.log(3);
+		valid = false;
+		badPass = badPass + " numbers,";
+	}
+	if(pass.length < 8) {
+		valid = false;
+		badPass = badPass + " not long enough (8 characters Minimum)";
+	}
+
+	console.log(valid);
+	if(valid === true){
     const newAdmin = new Admin({
         username: req.body.username,
         password: req.body.password, 
         firstName: req.body.firstName,
         lastName: req.body.lastName
     });
-    
     newAdmin.save().then(admin => res.json(admin));
+	}
+	else{
+	res.json(badPass);
+	}
+	valid = true;
 });
 
 
