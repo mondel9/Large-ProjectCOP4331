ed74b793437cbba8e8b1849ebd29400ea7226776
mondel9 <42776426+mondel9@users.noreply.github.com>
2018-11-13 15:09:03 -0500

    Update prescription.js

--- models/prescription.js
+++ models/prescription.js
@@ -4,16 +4,52 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PrescriptionSchema = new Schema({
-    Rx: String,
-    name: String,
-    quantity: Number,
-    instructions: String,
-    details: String,
-    datePrescribed: Date,
+    Rx: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,                // name of medication
+        required: true
+    },
+    quantity: {
+        type: String,               // total prescribed
+        required: true
+    },
+    dose: {
+        type: String,               // ex: 400 mg; 20 mL etc..
+        require: true
+    },
+    instructions: {
+        type: String,               // ex: take once a day in morning
+        required: true
+    },
+    details: {
+        type: String,               // ex: small, round, white tablets.
+        required: true
+    },
+    datePrescribed: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    numRefills: {
+        type: Number,
+        required: true
+    },
     refillDate: Date,
-    expirationDate: Date,
-    prescribedTo: mongoose.Schema.Types.ObjectId,
-    prescribedBy: mongoose.Schema.Types.ObjectId
+    expirationDate: {
+        type: Date,
+        required: true
+    },
+    prescribedTo: {
+        type: mongoose.Schema.Types.ObjectId,   // patientid or dependentid
+        required: true
+    },
+    prescribedBy: {
+        type: mongoose.Schema.Types.ObjectId,   // doctorid
+        required: true
+    }
 });
 
 module.exports = Prescription = mongoose.model('prescription', PrescriptionSchema);
