0191eeec143b703856ddeea4b94523d95d81f92e
Ayy3 <mauricetteaustin@gmail.com>
2018-11-28 13:14:04 -0500

    added unique usernames

--- models/admin.js
+++ models/admin.js
@@ -2,11 +2,13 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+mongoose.set('useCreateIndex', true);
 
 const AdminSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+		unique:true
     },
     password: {
         type: String,
